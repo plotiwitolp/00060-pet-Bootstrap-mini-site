907236fb285df6c91d8ffdb6e55d4c166325a3be
plotiwitolp <plotiwitolp@gmail.com>
2022-08-16 22:40:55 +0400

    first

---
+++ index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>00060-pet-Bootstrap-mini-site</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+</head>
+
+<body>
+    <div class="navbar navbar-dark bg-dark navbar-expand-lg navbar-fixed-top">
+        <div class="container">
+            <a href="#" class="navbar-brand">Logo</a>
+            <button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbar-1">
+                <span class="navbar-toggler-icon bg-dark"></span>
+            </button>
+            <div class="collapse navbar-collapse text-white" id="navbar-1">
+                <form class="navbar-form ms-auto d-lg-flex">
+                    <div class="form-group me-lg-2 mt-sm-3 mt-lg-0 mb-sm-1 mb-lg-0">
+                        <input type="text" placeholder="Email" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <input type="password" placeholder="Password" class="form-control">
+                    </div>
+                    <button type="submit" class="btn btn-success ms-lg-2  mt-sm-3 mt-lg-0 d-sm-flex ms-auto">Sign in</button>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <div class="bg-light p-3 rounded-md ">
+        <div class="container">
+            <h1>First title</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor blanditiis delectus nobis. Autem quidem, repellendus quos harum provident blanditiis maxime corrupti, illum, neque aspernatur placeat ipsum tempore sequi? Nemo, magni?
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor blanditiis delectus nobis. Autem quidem, repellendus quos harum provident blanditiis maxime corrupti, illum, neque aspernatur placeat ipsum tempore sequi? Nemo, magni?
+            </p>
+            <p>
+                <a href="#" class="btn btn-warning btn-lg mt-3">Simple link &raquo;</a>
+            </p>
+        </div>
+    </div>
+    <div class="container mt-3">
+        <div class="row">
+            <div class="col-md-4">
+                <h2>Second title 1</h2>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus reiciendis perferendis sint similique id? Numquam, aut amet hic voluptas tempora vero animi ipsam maiores expedita nemo magnam, aspernatur harum impedit!
+                </p>
+                <a href="#" class="btn btn-info mt-3">Another simple link &raquo;</a>
+            </div>
+            <div class="col-md-4">
+                <h2>Second title 2</h2>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus reiciendis perferendis sint similique id? Numquam, aut amet hic voluptas tempora vero animi ipsam maiores expedita nemo magnam, aspernatur harum impedit!
+                </p>
+                <a href="#" class="btn btn-info mt-3">Another simple link &raquo;</a>
+            </div>
+            <div class="col-md-4">
+                <h2>Second title 3</h2>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus reiciendis perferendis sint similique id? Numquam, aut amet hic voluptas tempora vero animi ipsam maiores expedita nemo magnam, aspernatur harum impedit!
+                </p>
+                <a href="#" class="btn btn-info mt-3">Another simple link &raquo;</a>
+            </div>
+        </div>
+    </div>
+    <hr>
+    <div class="container">
+        <div class="footer">
+            <p>
+                &copy; All right reserved 2022
+            </p>
+        </div>
+    </div>
+
+
+    <!-- JQuery -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <!-- Bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+    <script src="./assets/js/script.js"></script>
+</body>
+
+</html>
